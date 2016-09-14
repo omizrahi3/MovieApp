ce2fa391215348277fe1b516a69d0febe0ec8931
ODell Mizrahi <ODell Mizrahi>
2016-09-14 00:19:29 -0400

    stripe payment system implemented

--- Routes/user_api/index.js
+++ Routes/user_api/index.js
@@ -1,5 +1,8 @@
 var User = require('./../../Models/User.model');
 var bcrypt = require('bcryptjs');
+var stripe = require("stripe")(
+  "sk_test_UEiTmglWSL3WHQUZmhCcts5K"
+);
 
 function addUserToDB(eml, usern, psw, callback) {
   var hash = bcrypt.hashSync(psw, bcrypt.genSaltSync(10));
@@ -55,6 +58,22 @@ module.exports = function(app) {
         req.session.user = user;
         res.status(200).send(user);
       }
+    });
+  });
+
+  app.post('/charge', function(req, res){
+    stripe.charges.create({
+        amount: 2000,
+        currency: 'usd',
+        source: req.body.stripeToken,
+        description: 'One time deposit for ' + req.user.email + '.'
+      }, function(err, charge) {
+        if (err) return next(err);
+        req.user.balance += charge.amount;
+        req.user.save(function(err) {
+          if (err) return next(err);
+          res.redirect('/dashboard');
+        });
     });
   });
 
